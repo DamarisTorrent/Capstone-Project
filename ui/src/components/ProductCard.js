7f62da9f74685d740c69f93ea9c29e912eca07c9
Damaris Torrent <damaristorrent@gmail.com>
2023-03-31 20:12:22 -0400

    Product search on UI

--- ui/src/components/ProductCard.js
+++ ui/src/components/ProductCard.js
@@ -1,5 +1,4 @@
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import {Link, useNavigate } from 'react-router-dom'
@@ -64,5 +63,5 @@ export default function ProductCard(props) {
       </CardActions> */}
     </Card>
     
-  );
+  )
 }
--- ui/src/utility/utils.js
+++ ui/src/utility/utils.js
@@ -1,2 +1,16 @@
 // randomizes an array
 export const shuffle = (array) => array.sort((a, b) => 0.5 - Math.random())
+
+//These are women's wetsuit sizes
+export const displaySizes = ["0", "2", "4", "6", "8", "10", "12", "14", "16"]
+
+//Brands to be displayed in checkboxes
+export const displayBrands = [
+  "Rip Curl",
+  "O'Neill",
+  "Billabong",
+  "Roxy",
+  "Patagonia",
+  "Xcel",
+  "Sisstrevolution"
+]
