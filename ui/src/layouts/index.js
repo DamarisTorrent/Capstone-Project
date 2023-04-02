3487fa9df90343595d84938c0c1c50af71d37239
Damaris Torrent <damaristorrent@gmail.com>
2023-04-02 17:11:21 -0400

    Added mui appbar

--- ui/src/layouts/index.js
+++ ui/src/layouts/index.js
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
 import {Outlet} from "react-router-dom"
 import Footer from "./Footer";
+import Header from "./Header";
 
 const Layout = (props) => {
   return (
     <Fragment>
-     
+      <Header></Header>
       <Outlet/> {/* Outlet is not a “real” Component, it is imported from react-router-dom. it will be replaced by the element rendered by the route */}
       <Footer/>
     </Fragment>
